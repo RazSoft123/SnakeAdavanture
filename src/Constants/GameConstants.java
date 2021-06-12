686700fe861d119f8c9cf50a6295f8dd71ab2812
RazSoft <33057716+RazSoft123@users.noreply.github.com>
2021-06-12 18:54:25 +0530

    Contains constants used in side the game

---
+++ src/Constants/GameConstants.java
@@ -0,0 +1,19 @@
+//The class contains all game constants in the game.
+package Constants;
+
+import java.awt.Color;
+
+public class GameConstants {
+
+	public static final int W_WIDTH = 720;
+	public static final int W_Height = 480;
+	public static final String W_TITLE = "Razsoft Game Advanture";
+	public static final Color BG_COLOR = Color.GREEN;
+	public static final Color FONT_COLOR = Color.RED;
+	public static final long G_WAIT_TIME = 10l;						//General wait time
+	public static final int UP = 1;
+	public static final int DOWN = 2;
+	public static final int LEFT = 3;
+	public static final int RIGHT = 4;
+	
+}
